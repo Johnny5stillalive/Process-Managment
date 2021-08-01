f3d02068eff5118eec3eedd022cb715b1bcf3cac
Johnny5stillalive <jacobbridges3283@gmail.com>
2021-07-31 17:00:50 -0700

    Changed login style and tab title

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,21 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    color:black;
+    margin: 5px; 
+    
+`
 
 const LoginPage = () => (
 
         <>  
         <h1>Login</h1> 
-
-        <noscript class="comment">wrapping this block in a form causes it not to redirect but with out it wrapped we are not sending the form info</noscript>
+        <noscript className="comment">wrapping this block in a form causes it not to redirect but with out it wrapped we are not sending the form info</noscript>
          <form />  
-             <div class="container" >   
-                 <label>Username : </label>   
-                 <input type="text" placeholder="Enter Username" name="username" required />  
-                 <label>Password : </label>   
-                 <input type="password" placeholder="Enter Password" name="password" required />  
-                 <button type="submit" >Login</button> 
-                 <a href="/projectBoard"><input type="submit" value="fastLogin"></input></a>  
-                 <a href="/register"><input type="submit" value="register"></input></a>    
-            </div>
+             <Container >   
+               
+                    <label>Username : </label>   
+                    <input type="text" placeholder="Enter Username" name="username" required />  
+               </Container>
+                <Container>
+                    <label>Password : </label>   
+                    <input type="password" placeholder="Enter Password" name="password" required />  
+                <Container>
+                    <button type="submit" >Login</button> 
+                    <a href="/projectBoard"><input type="submit" value="fastLogin"></input></a>  
+                    <a href="/register"><input type="submit" value="register"></input></a>   
+                </Container>   
+            </Container>
                
          </>
 
